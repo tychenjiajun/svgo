b0a2ade819747b2fcd65c9820f8dc3ad89db295f
Bogdan Chadkin <trysound@yandex.ru>
2021-08-22 13:16:30 +0300

    Improve description

--- plugins/remove-xlink-ns.js
+++ plugins/remove-xlink-ns.js
@@ -4,13 +4,13 @@ exports.name = 'remove-xlink-ns';
 exports.type = 'visitor';
 exports.active = false;
 exports.description =
-  'removes xmlns:xlink and replaces xlink:href with href attribute';
+  'removes xmlns:xlink namespace and replaces xlink:href attributes with href';
 
 /**
- * removes xmlns:xlink from <svg> and replaces xlink:href with href attribute
+ * removes xmlns:xlink namespace and replaces xlink:href attributes with href
  *
- * xlink namespace is obsolete in SVG 2. Href attribute is recommended
- * replacement to xlink:href.
+ * xlink namespace is obsolete in SVG 2. Xlink:href is recommended to replace with
+ * href attribute.
  *
  * https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/href
  *
